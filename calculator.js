d278806507a181c496ee9609cb497f8421aa3c65
Krzysztof Miciuła <miciula@gmail.com>
2022-05-23 09:57:37 +0200

    variables added

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const numbers = document.querySelectorAll('.number')
+const mainResult = document.querySelector('.result')
+const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equal')
+const deleteEqual = document.querySelectorAll('.deleteEqual')
+const dot = document.querySelector('.dot')
+
+let result = ''
+let number1 = 0
+let number2 = 0
+let operator = undefined
