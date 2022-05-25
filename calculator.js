dd03377cde5ebc01892c3c5dba0126152645015e
Krzysztof Miciuła <miciula@gmail.com>
2022-05-25 10:25:34 +0200

    `equal`, `deleteEqual`,  `operators` And calc logic work

--- calculator.js
+++ calculator.js
@@ -5,33 +5,93 @@ const equal = document.querySelectorAll('.equal')
 const deleteEqual = document.querySelectorAll('.deleteEqual')
 const dot = document.querySelectorAll('.dot')
 const deleteLast = document.querySelectorAll('.deleteLast')
+const SPR = document.querySelectorAll('.SPR')
 
 let result = ''
-let number1 = 0
-let number2 = 0
+let number1 = ''
+let number2 = ''
 let operator = undefined
 
-
-add  = () => {
+count = () => {
+  switch (String(operator)) {
+    case '+': result = add(Number(number1), Number(number2))
+      break;
+    case '-': result = subtract(Number(number1), Number(number2))
+      break;
+    case '/': result = divide(Number(number1), Number(number2))
+      break;
+    case '*': result = multiply(Number(number1), Number(number2))
+      break;
+    default:
+      return
+  }
+}
+add = (a, b) => {
   return a + b
 }
-subtract = () => {
-  return a + b
+subtract = (a, b) => {
+  return a - b
 }
-divide = () => {
-  return a + b
+divide = (a, b) => {
+  return a / b
 }
-multiply = () => {
-  return a + b
+multiply = (a, b) => {
+  return a * b
 }
-
 concat = (number) => {
   result = result.toString() + number.toString()
   render()
 }
-
 render = () => {
   mainResult.innerText = result
+}
+
+check = () => {
+  if (result !== '' && result !== '-' && number1 === '' && number2 === '') {
+    number1 = result
+    result = 0
+    render()
+    result = ''
+    return
+  }
+  if (result !== '' && number1 !== '' && number2 === '') {
+    number2 = result
+    count()
+    render()
+    number1 = result
+    number2 = ''
+    result = ''
+    operator = undefined
+    return
+  }
+}
+
+changeOperator = (e) => {
+  if (number1 === '' && result === '' && operator === undefined) {
+    result = '-'
+    render()
+    return
+  }
+  if (number1 !== '' && result === '' && operator === undefined) {
+    operator = e
+    result = 0
+    render()
+    result = ''
+    return
+  }
+  if (number1 !== '' && result === '' && operator === '-') {
+    operator = e
+    result = '-'
+    render()
+    return
+  }
+  if (number1 !== '' && result === '' && operator !== undefined && operator !== '-') {
+    result = '-'
+    render()
+    return
+  }
+  operator = e
+  check()
 }
 
 numbers.forEach((numberFromButtonClick) => {
@@ -43,17 +103,11 @@ numbers.forEach((numberFromButtonClick) => {
   )
 })
 
-operators.forEach((operatorFromButtonClick) => {
-  operatorFromButtonClick.addEventListener(
+operators.forEach((e) => {
+  e.addEventListener(
     'click',
     () => {
-      result = ''
-      number1 = 0
-      number2 = 0
-      operator = operatorFromButtonClick.innerText
-      console.log(operator)
-      render()
-
+      changeOperator(e.innerText)
     }
   )
 })
@@ -62,11 +116,12 @@ deleteEqual.forEach((deleteEqualFromButtonClick) => {
   deleteEqualFromButtonClick.addEventListener(
     'click',
     () => {
-      result = ''
-      number1 = 0
-      number2 = 0
+      result = 0
+      number1 = ''
+      number2 = ''
       operator = undefined
       render()
+      result = ''
     }
   )
 })
@@ -85,8 +140,21 @@ equal.forEach((equalFromButtonClick) => {
   equalFromButtonClick.addEventListener(
     'click',
     () => {
-      // CONSOLE LOG !!
-      console.log(equalFromButtonClick.innerText)
+
+      if (result === '' && number1 !== '' && number2 === '') {
+        result = number1
+        operator = undefined
+        render()
+        result = ''
+        return
+      }
+      if (result !== '' && number1 === '' && number2 === '') {
+        render()
+        number1 = result
+        result = ''
+        return
+      }
+      check()
     }
   )
 })
@@ -97,6 +165,20 @@ dot.forEach((dotFromButtonClick) => {
     () => {
       // CONSOLE LOG !!
       console.log(dotFromButtonClick.innerText)
+    }
+  )
+})
+
+SPR.forEach((SPR) => {
+  SPR.addEventListener(
+    'click',
+    () => {
+      console.log(
+        'result: ', result,
+        'number1: ', number1,
+        'number2: ', number2,
+        'operator: ', operator
+      )
     }
   )
 })
